f568c6224f96b0708390e53678143df7efae71d6
Kevin Wessa <kjwessa@gmail.com>
2019-07-29 15:29:03 -0500

    Polishing sticky menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,11 +2,25 @@
 
 /* eslint-env jquery */
 
-$('document').ready(function() {
-   
-    $('h1').click(function() {
-       $(this).css('background-color', '#ff0000');
-   }) 
-    
-    
+$(document).ready(function() {
+
+    $('.js--section-features').waypoint(function (direction) {
+            if (direction == "down") {
+                $('nav').addClass('sticky');
+            } else {
+                $('nav').removeClass('sticky');
+            }
+    }, {
+        offset: '60px;'
+    });
+
+
+    /*
+        var waypoint = new Waypoint({
+      element: document.getElementById('px-offset-waypoint'),
+      handler: function(direction) {
+        notify('I am 20px from the top of the window')
+      },
+      offset: 20  */
+
 });
