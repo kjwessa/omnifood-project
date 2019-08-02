ab8f236ebb035f237bbbb91fc2b83305292fe3fa
Kevin Wessa <kjwessa@gmail.com>
2019-08-02 10:52:29 -0500

    Entrance animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -41,10 +41,6 @@ $(document).ready(function () {
     })
 
     /*navigation scroll*/
-
-
-
-    // Select all links with hashes
     $('a[href*="#"]')
         // Remove links that don't actually link to anything
         .not('[href="#"]')
@@ -79,4 +75,31 @@ $(document).ready(function () {
                 }
             }
         });
+
+    /* Animations on Scroll */
+
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    })
+    
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    })
+    
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    })
+    
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    })
+
 });
