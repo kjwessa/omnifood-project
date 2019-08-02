5deacba782ef10116b1ba24a9d6e941a6e826a56
Kevin Wessa <kjwessa@gmail.com>
2019-08-02 11:47:30 -0500

    Responsive Nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -101,5 +101,22 @@ $(document).ready(function () {
     }, {
         offset: '50%'
     })
+    
+    $('.js--nav-icon').click(function() {
+        var nav = $('.main-nav');
+        
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200)
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+        
+        
+        ion-close-round
+    });
 
 });
